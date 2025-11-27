5ba2dc9f6a6b0fd64b7843ecab133d57d79514bb
jose rodriguez <72013964+eljoserass@users.noreply.github.com>
2025-11-27 18:34:15 +0100

    Update train_ann_rgb.py

--- train_ann_rgb.py
+++ train_ann_rgb.py
@@ -1,5 +1,5 @@
 """
 - define the model architecture
 - try to do inference on one sample and figure it out the input output
-
+dataset link https://kdrive.infomaniak.com/app/share/975517/613e58d5-3c60-4c52-a57f-a5871c00167f/files/51
 """
